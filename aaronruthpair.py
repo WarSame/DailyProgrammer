62a1d191f6784c5e1cb75572b4d724af4d889349
Graeme Cliffe <graemecliffe@gmail.com>
2015-10-26 18:45:02 -0400

    Fixed distinct primes problem

--- aaronruthpair.py
+++ aaronruthpair.py
@@ -5,18 +5,17 @@ def findFactors(n):
     factors=[]
     f=2
     while n!=1:
-        if (n%f)==0:
-            factors.append(f)
+        while (n%f)==0:
+            if f not in factors:
+                factors.append(f)
             n/=f
-        else:
-            f+=1
+        f+=1
 
     return factors
 
 def readInput():
-    numPairs=int(raw_input("Enter number of pairs:"))
     pairs=[]
-    for i in range(0,numPairs):
+    for i in range(0,int(raw_input("Enter number of pairs:"))):
         pair = raw_input().strip("(").strip(")")
         pairSplit = [int(n) for n in pair.split(",")]
         pairs.append(pairSplit)
@@ -27,8 +26,6 @@ def main():
     for i in pairs:
         fact1=findFactors(i[0])
         fact2=findFactors(i[1])
-        print fact1
-        print fact2
         print i,
         print "VALID" if (abs(i[0]-i[1])==1 and sum(fact1)==sum(fact2)) else "NOT VALID"
 
