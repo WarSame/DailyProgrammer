dedc7da3ef41575470232effb2481c1c71101dc6
Graeme Cliffe <graemecliffe@gmail.com>
2015-10-27 01:21:01 -0400

    Bag system, fix for consonant/vowel

--- #238-ConsonantsAndVowels/consonantsandvowels.py
+++ #238-ConsonantsAndVowels/consonantsandvowels.py
@@ -1,21 +1,11 @@
 #Consonants and Vowels Challenge
 #Graeme Cliffe
 
-import random
-
-def randChar(formatChar):
-    vow=list("aeiou")
-    con=list("bcdfghjklmnpqrstvwxyz")
-    vowUp=list(c.upper() for c in vow)
-    conUp=list(c.upper() for c in con)
-    strDict={'c':random.choice(con), 'C':random.choice(conUp),
-             'v':random.choice(vow), 'V':random.choice(vowUp)}
-    return strDict.get(formatChar) or 'ERROR'
+from random import choice
 
 def main():
-    outstring=[]
-    for i in raw_input():
-        outstring.append(randChar(i))
-    print ''.join(outstring)
+    strDict={'c':"bcdfghjklmnpqrstvwxyz",'v':"aeiou"}
+    strDict['C'],strDict['V']=strDict['c'].upper(),strDict['v'].upper()
+    print ''.join((choice(strDict[j])) for j in raw_input() )
 
 main()
