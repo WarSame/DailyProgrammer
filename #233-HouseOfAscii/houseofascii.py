e30d7f9ddf44bd13816c03b363840e6d393c9c74
Graeme Cliffe <graemecliffe@gmail.com>
2015-10-29 01:40:40 -0400

    Spacing issues on asciihouse partly fixed, still blank line if there is adjoining identical units

--- #233-HouseOfAscii/houseofascii.py
+++ #233-HouseOfAscii/houseofascii.py
@@ -55,10 +55,6 @@ def pullAdj(levels,x,y):#return list of nearby units
                     adj.append(" ")
     return adj
 
-def printHouse(levels,numLevels):
-    for i in range(0,numLevels):
-        printRow(i,levels)
-
 def getInput():
     #Take input from user - number of levels, then level layout
     numLevels=int(raw_input())
@@ -67,20 +63,41 @@ def getInput():
         levels.append(list(raw_input()))
     return numLevels,levels
 
-def printRow(n,levels):
+def printRow(n,levels,printTop,printBot):
     #Print a row given the row number and the layout array
     rowStrings=['','','']
     for i in range(len(levels[n])):
-        boxLists=formatBox(levels[n][i],pullAdj(levels,n,i))#list of sub-row strings
+        #list of sub-row strings
+        boxLists=formatBox(levels[n][i],pullAdj(levels,n,i))
         rowStrings[0]+=boxLists[0]
         rowStrings[1]+=boxLists[1]
         rowStrings[2]+=boxLists[2]
-    for i in rowStrings:
-        print i
+    if printTop:
+        print rowStrings[0]
+    print rowStrings[1]
+    if printBot:
+        print rowStrings[2]
+        
+def printHouse(levels,numLevels):
+    for i in range(0,numLevels):
+        printTop=False
+        printBot=False
+        if i-1<0:#if the above level doesn't exist
+            printTop=True
+        elif levels[i]!=levels[i-1]:
+            #if current level and previous have different layouts
+            #then don't print the seperators
+            printTop=True
+        if i+1>numLevels-1:
+            printBot=True
+        elif levels[i]!=levels[i+1]:
+            #if current level and next have different layouts
+            #don't print seperators
+            printBot=True
+        printRow(i,levels,printTop,printBot)
 
 def main():
     numLevels,levels=getInput()
-
     printHouse(levels,numLevels)
 
 main()
