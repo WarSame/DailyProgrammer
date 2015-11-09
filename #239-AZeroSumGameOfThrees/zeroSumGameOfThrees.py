b0ff848aed386c662b1c54320d27bc16ad7efe97
Graeme Cliffe <graemecliffe@gmail.com>
2015-11-08 21:27:09 -0500

    Progress on zero sum game of three - returns values correctly, but answers seems off slightly

--- #239-AZeroSumGameOfThrees/zeroSumGameOfThrees.py
+++ #239-AZeroSumGameOfThrees/zeroSumGameOfThrees.py
@@ -1,51 +1,56 @@
 #A Zero-Sum Game Of Threes Challenge
 #Graeme Cliffe
 
-class node:
-    children=list()
-    additions=list()
-    currTotal=0#Value remaining from original number
-    currSum=0#Sum of additions so far
-    visited=False
-    def __init__(self):
-        self.children=[2,1,-1,-2]
-    def setValues(self,additions,currSum):
-        self.children=[2,1,-1,-2]
-        self.additions=additions
-        self.currSum=currSum
-    def setTotal(self,currTotal):
-        self.currTotal=currTotal
-    def getTotal(self):
-        return self.currTotal
-    def getChild(self):
-        if len(self.children)==0:
-            return None
-        return self.children.pop()
+children=[-2,-1,1,2]
+
+def dfsChildren(parentRunTot,parentSum):#spin children off of parent node
+    #print "Checking children"
+    for childVal in children:#Attempt all of the children branches
+        print "New child: parent value total=",parentRunTot
+        print "Childval is",childVal
+        
+        childSum=parentSum+childVal
+        childRunTot=parentRunTot+childVal
+        
+        retBool,additionList=dfs(childRunTot,childSum)
+
+        if retBool:#If this child returns True, then append and return up
+            print "Returning",additionList
+            additionList.append(childVal)
+            return True, additionList
+    print "No case matched"
+    return False,0#No case matched
 
 #Use a DFS. If we reach more than 3*initial number a solution is impossible
-def dfs(currNode):
-    if currNode.currTotal==1:
-        return True
-    if currNode.currTotal>3*initNum:
-        return False
-    if currNode.currTotal<1:
-        return False
-    childRets=list()
-    for child in currNode.children:#Attempt all of the children branches
-        childVal=currNode.getChild()
-        childNode=node()
-        
-        childAdditions=currNode.additions
-        childAdditions.append(childVal)
-        childSum=currNode.currSum+childVal
-        childNode.setTotal(currNode.getTotal()+childVal)
-        childNode.setValues(childAdditions,childSum)
-        
-        retVal=dfs(childNode)
-        childRets.append(retVal)
-    return any(childRets)
+def dfs(parentRunTot,parentSum):
+    print "=====Entering dfs====="
+    print "Current value is",parentRunTot
+    print "Current sum is",parentSum
+    if parentRunTot==1:
+        if parentSum==0:
+            #If we have hit the base case
+            print "Base case"
+            baseList=list()
+            baseList.append(0)
+            return True,baseList
+        return False,0#End of branch
+    if parentRunTot>3*initNum:
+        #If we reach too high, end this child
+        print "Too high"
+        return False,0
+    if parentRunTot<1:
+        #If we move below 1, end this child
+        print "Below 1"
+        return False,0
+    if parentRunTot%3==0:
+        #If we can divide by 3, we do
+        print "Dividing by 3"
+        parentRunTot/=3
+        return dfsChildren(parentRunTot,parentSum)
+    #If we aren't at the base case, but no problems, keep checking children
+    else:
+        print "Normal children"
+        return dfsChildren(parentRunTot,parentSum)
 
 initNum=int(raw_input())
-currNode=node()
-currNode.setTotal(initNum)
-print dfs(currNode)
+print dfs(initNum,0)
