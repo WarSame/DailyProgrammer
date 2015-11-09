e94ed281ac6da9b3933ed8c3fdfbf502f7c243cd
Graeme Cliffe <graemecliffe@gmail.com>
2015-11-09 13:27:24 -0500

    Finished printing /3s, removed diagnostic prints. Problem finished.

--- #239-AZeroSumGameOfThrees/zeroSumGameOfThrees.py
+++ #239-AZeroSumGameOfThrees/zeroSumGameOfThrees.py
@@ -14,25 +14,15 @@ def isInTable(nodeTot,nodeSum):
         return False
 
 def dfsChildren(parentRunTot,parentSum):#spin children off of parent node
-    #print "Checking children"
     for childVal in children:#Attempt all of the children branches
         childSum=parentSum+childVal
         childRunTot=parentRunTot+childVal
-
-        #print "=====Printing Adult====="
-        #print "Adult total is",parentRunTot
-        #print "Adult sum is",parentSum
-        #print "=====Printing Child===="
-        #print "Childval is",childVal
-        #print "ChildTot is",childRunTot
-        #print "ChildSum is",childSum
         
         additionList=dfs(childRunTot,childSum)
         if additionList:#If this child returns True, then append and return up
-            #print "Returning",additionList
+
             additionList.append(childVal)
             return additionList
-    #print "No children of this branch work"
     return None#No case matched
 
 #Use a DFS. If we reach more than 3*initial number a solution is impossible
@@ -41,44 +31,39 @@ def dfs(parentRunTot,parentSum):
     #It must not work - return false
     if isInTable(parentRunTot,parentSum):
         return False
-    #print "=====Entering dfs====="
-    #print "Current total is",parentRunTot
-    #print "Current sum is",parentSum
     if parentRunTot==1:
         if parentSum==0:
             #If we have hit the base case
-            #print "Base case"
             baseList=list()
             baseList.append(0)
             return baseList
-        #print "End of Branch"
         return None#End of branch
     if parentRunTot>3*initNum:
         #If we reach too high, end this child
-        #print "Too high"
         return None
     if parentRunTot<1:
         #If we move below 1, end this child
-        #print "Below 1"
         return None
     if parentRunTot%3==0:
         #If we can divide by 3, we do
-        #print "Dividing by 3"
         parentRunTot/=3
-        return dfs(parentRunTot,parentSum)
+        retList=dfs(parentRunTot,parentSum)
+        if retList:#If we get returned a list of addition/subtraction
+            retList.append("/3")
+        return retList
     #If we aren't at the base case, but no problems, keep checking children
     else:
-        #print "Normal children"
         return dfsChildren(parentRunTot,parentSum)
 
 initNum=int(raw_input())
 result=None
-try:
+try:#Recursion error on impossible solution
     result=dfs(initNum,0)
     result=list(reversed(result))
 except:
     result=None
 if result:
+    print "Starting with:",initNum
     print "Additions and subtractions in order:"
     for i in result:
         print i,
