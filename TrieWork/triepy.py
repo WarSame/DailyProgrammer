eff8a6fa48749e4efe6a8b1bf4cc0d57318c873d
Graeme Cliffe <graemecliffe@gmail.com>
2015-11-17 23:02:09 -0500

    This is what the last commit was supposed to have.

--- TrieWork/triepy.py
+++ TrieWork/triepy.py
@@ -18,6 +18,10 @@ class Node:
         if char in self.children:
             return self.children[char]
 
+    def get_children(self):
+        for i in self.children:
+            print i
+'''
     def __str__(self):
         node_string = self.word
         if not self.children.keys():
@@ -26,7 +30,7 @@ class Node:
         for char in self.children.keys():
             node_string += char + "\t"
         return node_string
-
+'''
 
 class Trie:
     def __init__(self, words):
@@ -36,25 +40,56 @@ class Trie:
             self.insert_word(word)
 
     def print_children(self, stub):
-        # Given a stub of a word, print all words from it
+        # Given a stub of a word, print all words following from it
         curr_node = self.start_node
         for char in stub:
-            pass
+            child = curr_node.get_child(char)
+            if child:
+                # If our node has a child, move to it and continue
+                curr_node = child
+            else:
+                # If not, the stub is not in the trie
+                return ""
+        # We now have our stub node and can find all
+        # Its descendants, and return those
+        leaves = self.get_children_leaves(curr_node)
+        for i in range(0,len(leaves)):
+            leaves[i] = stub + leaves[i]
+        return leaves
+
+    def get_children_leaves(self, curr_node):
+        leaves = []
+        for child in curr_node.children.keys():
+            print "Child is "+child
+            child_node = curr_node.children[child]
+
+            # Add children leaves to our leaves collection
+            # Get the leaves of our children
+            leaves = self.get_children_leaves(child_node)
+            # Append the current letter in front of all child leaves
+            for i in range(0,len(leaves)):
+                leaves[i] = child + leaves[i]
+
+            if child_node.leaf:
+                leaves.append(child)
+        # Return leaves up
+        return leaves
 
     def insert_word(self, word):
         # Given a word, insert it into the trie
         curr_node = self.start_node
         for char in word:
             # Loop through entire word
-            prev_node = curr_node
-            child = prev_node.get_child(char)
+            child = curr_node.get_child(char)
             if child:
                 # If you find a child, move to it
                 curr_node = child
             else:
                 # If not, make one
-                curr_node = Node(prev_node.word)
-                prev_node.set_child(char, curr_node)
+                new_node = Node(curr_node.word)
+                curr_node.set_child(char, new_node)
+                #And move to that new one
+                curr_node = new_node
         # Make the last character in the word a leaf
         curr_node.leaf = True
 
@@ -68,9 +103,9 @@ class Trie:
                 curr_node = child
             else:
                 # Otherwise the word is not in the trie
-                return False, ""
+                return False
         # Return whether the end node is a leaf or not
-        return curr_node.leaf, str(curr_node)
+        return curr_node.leaf
 
     def is_in_trie(self, word):
         # Given a word, find if it is in the trie
@@ -86,13 +121,17 @@ class Trie:
 
 
 def main():
+    '''
     f = open("../enable1.txt","r")
     words=[]
     for line in f.readlines():
         words.append(line.strip("\n"))
     tr = Trie(words)
-    result = tr.is_in_trie("a")
+    '''
+    tr = Trie(["this","theory","that"])
+    result = tr.print_children("th")
     print result
+
 
 
 main()
